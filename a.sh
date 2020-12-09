ac9df6e3c7b3f24dd092ed75e8b9f25f5c10f2de
Naman <64313682+cyclo-alkane@users.noreply.github.com>
2020-12-09 11:57:12 +0530

    Update a.sh

--- a.sh
+++ a.sh
@@ -10,20 +10,21 @@ printf "██╔══██║██║╚██╗██║██╔══█
 printf "██║  ██║██║ ╚████║██║  ██║███████╗██║   ███████║██║███████║ \n"
 printf "╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝   ╚══════╝╚═╝╚══════╝ \n"
                                                                          
-                                                                                                              
-                                                              
+                                                                                                                                                                       
                                                          
 printf "\n"
 }
 menu() {
 
-printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m To see ETHERNET-HEADER\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m To see ETHERNET-HEADER\e[0m\n"
 
-printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m To see IP HEADER\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m To see all TCP packets\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m To see all UDP packets \e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m To see all ICMP packets\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m To see all MISC packets\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m To see IP HEADER\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m To see all TCP packets\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m04\e[0m\e[1;92m]\e[0m\e[1;93m To see all UDP packets \e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m05\e[0m\e[1;92m]\e[0m\e[1;93m To see all ICMP packets\e[0m\n"
+
+
+printf "\e[1;92m[\e[0m\e[1;77m07\e[0m\e[1;92m]\e[0m\e[1;93m To see all MISC packets\e[0m\n"
 printf "\n"
 printf "\e[1;93m[\e[0m\e[1;77m99\e[0m\e[1;93m]\e[0m\e[1;77m Exit\e[0m\n"
 printf "\n"
@@ -33,25 +34,23 @@ if [[ $option == 1 || $option == 01 ]]; then
 
 awk '$0 == "Ethernet Header" {i=1;next};i && i++ <= 18' sniffed.txt
 elif [[ $option == 2 || $option == 02 ]]; then
-cd analysis
+
 awk '$0 == "IP Header" {i=0;next};i && i++ <= 18' sniffed.txt
+
 elif [[ $option == 3 || $option == 03 ]]; then
-cd analysis
 awk '$0 == "***********************TCP Packet*************************" {i=1;next};i && i++ <= 18' sniffed.txt
+
 elif [[ $option == 4 || $option == 04 ]]; then
-cd analysis
 awk '$0 == "***********************UDP Packet*************************" {i=1;next};i && i++ <= 18' sniffed.txt
+
 elif [[ $option == 5 || $option == 05 ]]; then
-cd analysis
 awk '$0 == "***********************UDP Packet*************************" {i=1;next};i && i++ <= 18' sniffed.txt
+
 elif [[ $option == 6 || $option == 06 ]]; then
-cd analysis
 awk '$0 == "***********************ICMP Packet*************************" {i=1;next};i && i++ <= 18' sniffed.txt
-elif [[ $option == 7 || $option == 07 ]]; then
-cd analysis
-awk '$0 == "***********************IGMP Packet*************************" {i=1;next};i && i++ <= 18' sniffed.txt
+
+
 elif [[ $option == 3 || $option == 03 ]]; then
-cd analysis
 awk '$0 == "***********************TCP Packet*************************" {i=1;next};i && i++ <= 18' sniffed.txt
  
 
@@ -61,6 +60,7 @@ else
 printf "\e[5;93m[\e[1;77m!\e[0m\e[1;93m] Invalid option!\e[0m"
 sleep 0.5
 clear
+banner
 menu
 fi
 }
